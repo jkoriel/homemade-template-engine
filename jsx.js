b2fe5089864c8f464d78ec37ab5bfe909479f704
joseph koriel <josephkoriel@josephs-MacBook-Pro.local>
2019-06-04 18:53:50 -0700

    complete

--- jsx.js
+++ jsx.js
@@ -3,15 +3,28 @@
 
 // variables to look for
 let stuff = {
-  topHeader: 'the new top header',
-  linkText: 'the new link text',
-  finalMessage: 'this is the final message'
-}
+  topHeader: "the new top header",
+  linkText: "the new link text",
+  finalMessage: "this is the final message"
+};
 
 // the string of html to look through
-let thebody = document.body.innerHTML
+let thebody = document.body.innerHTML;
+
+// console.log("BODY", thebody);
+// thebody = thebody.replace("{topHeader}", stuff.topHeader);
+// thebody = thebody.replace("{linkText}", stuff.linkText);
+// thebody = thebody.replace("{finalMessage}", stuff.finalMessage);
+
+//other way to do it more JSX
+
+for (let key in stuff) {
+  thebody = thebody.replace(key, stuff[key]);
+}
+
+thebody = thebody.replace(/{|}/g, "");
 
 // YOUR CODE HERE...
 
 // replacing the existing HTML string with the new HTML string
-document.body.innerHTML = thebody
+document.body.innerHTML = thebody;
